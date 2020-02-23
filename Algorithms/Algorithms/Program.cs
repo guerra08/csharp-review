3f4c630fb4cdffb4093f787710e2aa43f3001a5b
Bruno Guerra <g.g.bruno.00@gmail.com>
2020-02-23 15:30:14 -0300

    Algorithms; bubble

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting s = new Sorting();
+            int[] myArray = {1,2,3,4,5,8,7};
+            s.BubbleSort(myArray);
+            PrintArray(myArray);
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Sorting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Sorting
+    {
+        public void BubbleSort(int []array)
+        {
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            int n = array.Length;
+            int count = 0;
+            Boolean swap = false;
+
+            while (count < n)
+            {
+                if (count == n - 1 && swap == false)
+                {
+                    break;
+                }
+
+                if (count == n - 1 && swap == true)
+                {
+                    swap = false;
+                    count = 0;
+                }
+
+                if (array[count] > array[count + 1]) {
+                    int aux = array[count + 1];
+                    array[count + 1] = array[count];
+                    array[count] = aux;
+                    swap = true;
+                }
+
+                count++;
+            }
+        }
+
+    }
+}
