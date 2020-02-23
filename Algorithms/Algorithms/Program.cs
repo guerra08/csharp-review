abc8b33ecf9e4abe479b5baca3f269aa5db3ad41
Bruno Guerra <g.g.bruno.00@gmail.com>
2020-02-23 16:00:26 -0300

    Insertion

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -8,16 +8,20 @@ namespace Algorithms
         {
             Sorting s = new Sorting();
             int[] myArray = {1,2,3,4,5,8,7};
+            int[] other = { 2, 1, 5, 6, 9, 3 };
             s.BubbleSort(myArray);
-            PrintArray(myArray);
+            //PrintArray(myArray);
+            s.InsertionSort(other);
+            PrintArray(other);
         }
 
         static void PrintArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(array[i]);
+                Console.Write(array[i] + " ");
             }
+            Console.WriteLine();
         }
     }
 }
--- Algorithms/Algorithms/Sorting.cs
+++ Algorithms/Algorithms/Sorting.cs
@@ -41,5 +41,21 @@ namespace Algorithms
             }
         }
 
+        public void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
+
     }
 }
