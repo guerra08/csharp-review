f7e356041e3c96b014e3b41d8ac5049f8558dc83
Bruno Guerra <g.g.bruno.00@gmail.com>
2020-02-24 20:21:30 -0300

    Linked List (double linked)

---
+++ DataStructures/DataStructures/LinkedList.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class LinkedList
+    {
+
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public LinkedList(Node first)
+        {
+            Head = first;
+            Tail = first;
+            Head.Next = Tail;
+            Tail.Prev = Head;
+            Count = 1;
+        }
+
+        public LinkedList(int val)
+        {
+            Node n = new Node(val);
+            Head = n;
+            Tail = n;
+            Head.Next = Tail;
+            Tail.Prev = Head;
+            Count = 1;
+        }
+
+        public void Add(int val)
+
+        {
+            Node n = new Node(val);
+
+            if(Count == 0)
+            {
+                Head = n;
+                Tail = n;
+                Head.Next = Tail;
+                Tail.Prev = Head;
+                Count++;
+            }
+            else if(Count == 1)
+            {
+                Head.Next = n;
+                Tail = n;
+                Tail.Prev = Head;
+                Count++;
+            }
+            else
+            {
+                Node aux = Tail;
+                Tail = n;
+                Tail.Prev = aux;
+                aux.Next = Tail;
+                Count++;
+            }
+
+        }
+
+        public Boolean Exists(int val)
+        {
+            Node n = new Node(val);
+
+            if (Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                int aux = 0;
+                Node check = Head;
+                while (aux < Count)
+                {
+                    if (check.Val == n.Val)
+                    {
+                        return true;
+                    }
+                    check = check.Next;
+                    aux++;
+                }
+                return false;
+            }
+        }
+
+        public void PrintList()
+        {
+            if(Count == 0)
+            {
+                Console.WriteLine("Empty list");
+            }
+            else
+            {
+                int c = 0;
+                Node n = Head;
+                while(c < Count)
+                {
+                    if(c == Count - 1)
+                    {
+                        Console.WriteLine(n.Val);
+                    }
+                    else
+                    {
+                        Console.Write(n.Val + " - ");
+                    }
+                    c++;
+                    n = n.Next;
+                }
+            }
+        }
+
+        public class Node
+        {
+            public int Val { get; set; }
+            public Node Prev { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int val)
+            {
+                Val = val;
+                Next = null;
+                Prev = null;
+            }
+
+            public Node(int val, Node prev, Node next)
+            {
+                Val = val;
+                Prev = prev;
+                Next = next;
+            }
+
+        }
+
+    }
+}
--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -6,14 +6,21 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            BinTree t = new BinTree(15);
-            t.AddOnFree(20);
-            t.AddOnFree(25);
-            t.AddOnFree(35);
-            t.AddOnFree(256);
-            t.PreFixLeft(t.Head);
-            Console.WriteLine("\n------");
-            t.PreFixRight(t.Head);
+            //BinTree t = new BinTree(15);
+            //t.AddOnFree(20);
+            //t.AddOnFree(25);
+            //t.AddOnFree(35);
+            //t.AddOnFree(256);
+            //t.PreFixLeft(t.Head);
+            //Console.WriteLine("\n------");
+            //t.PreFixRight(t.Head);
+
+            LinkedList l = new LinkedList(10);
+            l.Add(25);
+            l.Add(50);
+            l.Add(15);
+            l.PrintList();
+            Console.WriteLine(l.Exists(50));
 
         }
     }
